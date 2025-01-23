cd82406028c610c2912cb19fa850c5dc3924e8df
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-01-23 17:32:28 +0700

    ADJUST JUMBOTRON HEIGHT

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,15 +3,15 @@ import JumbotronImage from "../assets/images/jumbotron-image.jpg"
 const Jumbotron = () => {
     return (
         <header
-            className="h-[50vh] bg-center bg-cover bg-no-repeat bg-gray-700 bg-blend-multiply flex flex-col justify-center items-center gap-4 rounded-lg shadow mb-4"
+            className="h-[30vh] md:h-[50vh] bg-center bg-cover bg-no-repeat bg-gray-700 bg-blend-multiply flex flex-col justify-center items-center gap-4 rounded-lg shadow p-4 mb-4"
             style={{
                 backgroundImage: `url(${JumbotronImage})`,
             }}
         >
-            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+            <h1 className="mb-4 text-4xl text-center font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
                 DreamCommerce
             </h1>
-            <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+            <p className="text-lg text-center font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
                 Turning Your Dream Purchases into Reality
             </p>
         </header>
