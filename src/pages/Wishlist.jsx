b0e7cba72dd5e6b796ac114f10920d56bd06622f
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-01-23 17:34:18 +0700

    CHANGING GRID COLUMN WISHLIST

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -9,7 +9,7 @@ const Wishlist = () => {
         <section className="container min-h-[90vh] py-4">
             <NavBack />
             <h2 className="mb-8">Wishlist</h2>
-            <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+            <div className="w-full grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
             {wishlist.length > 0 ? wishlist.map(product => (
                 <CardProduct key={product.id} product={product} />
             )) : 
