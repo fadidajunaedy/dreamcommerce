d17569d31f02cf6f145ceabd67daefd55dd4c08f
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-01-23 18:01:26 +0700

    CHANGE METHOD OF NAVBACK USING NAVIGATE

--- src/components/NavBack.jsx
+++ src/components/NavBack.jsx
@@ -1,8 +1,11 @@
 import { HiArrowLongLeft } from "react-icons/hi2"
+import { useNavigate } from "react-router-dom"
 
 const NavBack = () => {
+    const navigate = useNavigate()
+
     return (
-        <button type="button" onClick={() => window.history.back()} className="w-full cursor-pointer mb-4">
+        <button type="button" onClick={() => navigate(-1)} className="w-full cursor-pointer mb-4">
             <span className="flex items-center">
                 <HiArrowLongLeft size={18}/>
                 &nbsp;
