cb6e7b43fb2e8afe45d96e6de63a56faab10feef
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2025-01-25 11:03:58 +0700

    ADDING PERSIST STORATE FOR STATE

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { store } from './store/index.js'
+import { store, persistor } from './store/index.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 )
--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,28 @@ import { configureStore } from "@reduxjs/toolkit"
 import cartReducer from "./cartSlice"
 import wishlistReducer from "./wishlistSlice"
 
+import storage from "redux-persist/lib/storage"
+import { persistStore, persistReducer } from "redux-persist"
+
+const persistCartConfig = {
+    key: "cart",
+    storage
+}
+
+const persistWishlistConfig = {
+    key: "wishlist",
+    storage
+}
+
+const persistedCart = persistReducer(persistCartConfig, cartReducer)
+const persistedWishlist = persistReducer(persistWishlistConfig, wishlistReducer)
+
 export const store = configureStore({
     reducer: {
-        cart: cartReducer,
-        wishlist: wishlistReducer
+        cart: persistedCart,
+        wishlist: persistedWishlist
     },
     devTools: true
 })
+
+export const persistor = persistStore(store)
